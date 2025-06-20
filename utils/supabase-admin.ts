b2f34d3613aa858406d06e8066e36c0948f6e839
Aristide <aristide.pichereau@orange.com>
2025-06-20 17:44:59 +0200

    refactor: comment out unused functions and imports in webhook and supabase-admin modules

--- utils/supabase-admin.ts
+++ utils/supabase-admin.ts
@@ -9,101 +9,101 @@ type Price = Database['public']['Tables']['prices']['Row'];
 
 // Note: supabaseAdmin uses the SERVICE_ROLE_KEY which you must only use in a secure server-side context
 // as it has admin privileges and overwrites RLS policies!
-const supabaseAdmin = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-  process.env.SUPABASE_SERVICE_ROLE_KEY || ''
-);
+// const supabaseAdmin = createClient<Database>(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+//   process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+// );
 
-const upsertProductRecord = async (product: Stripe.Product) => {
-  const productData: Product = {
-    id: product.id,
-    active: product.active,
-    name: product.name,
-    description: product.description ?? null,
-    image: product.images?.[0] ?? null,
-    metadata: product.metadata
-  };
+// const upsertProductRecord = async (product: Stripe.Product) => {
+//   const productData: Product = {
+//     id: product.id,
+//     active: product.active,
+//     name: product.name,
+//     description: product.description ?? null,
+//     image: product.images?.[0] ?? null,
+//     metadata: product.metadata
+//   };
 
-  const { error } = await supabaseAdmin.from('products').upsert([productData]);
-  if (error) throw error;
-  console.log(`Product inserted/updated: ${product.id}`);
-};
-
-const upsertPriceRecord = async (price: Stripe.Price) => {
-  const priceData: Price = {
-    id: price.id,
-    product_id: typeof price.product === 'string' ? price.product : '',
-    active: price.active,
-    currency: price.currency,
-    description: price.nickname ?? null,
-    type: price.type,
-    unit_amount: price.unit_amount ?? null,
-    interval: price.recurring?.interval ?? null,
-    interval_count: price.recurring?.interval_count ?? null,
-    trial_period_days: price.recurring?.trial_period_days ?? null,
-    metadata: price.metadata
-  };
-
-  const { error } = await supabaseAdmin.from('prices').upsert([priceData]);
-  if (error) throw error;
-  console.log(`Price inserted/updated: ${price.id}`);
-};
-
-// const createOrRetrieveCustomer = async ({
-//   email,
-//   uuid
-// }: {
-//   email: string;
-//   uuid: string;
-// }) => {
-//   const { data, error } = await supabaseAdmin
-//     .from('customers')
-//     .select('stripe_customer_id')
-//     .eq('id', uuid)
-//     .single();
-//   if (error || !data?.stripe_customer_id) {
-//     // No customer record found, let's create one.
-//     const customerData: { metadata: { supabaseUUID: string }; email?: string } =
-//       {
-//         metadata: {
-//           supabaseUUID: uuid
-//         }
-//       };
-//     if (email) customerData.email = email;
-//     const customer = await stripe.customers.create(customerData);
-//     // Now insert the customer ID into our Supabase mapping table.
-//     const { error: supabaseError } = await supabaseAdmin
-//       .from('customers')
-//       .insert([{ id: uuid, stripe_customer_id: customer.id }]);
-//     if (supabaseError) throw supabaseError;
-//     console.log(`New customer created and inserted for ${uuid}.`);
-//     return customer.id;
-//   }
-//   return data.stripe_customer_id;
+//   const { error } = await supabaseAdmin.from('products').upsert([productData]);
+//   if (error) throw error;
+//   console.log(`Product inserted/updated: ${product.id}`);
 // };
 
-/**
- * Copies the billing details from the payment method to the customer object.
- */
-const copyBillingDetailsToCustomer = async (
-  uuid: string,
-  payment_method: Stripe.PaymentMethod
-) => {
-  //Todo: check this assertion
-  const customer = payment_method.customer as string;
-  const { name, phone, address } = payment_method.billing_details;
-  if (!name || !phone || !address) return;
-  //@ts-ignore
-  await stripe.customers.update(customer, { name, phone, address });
-  const { error } = await supabaseAdmin
-    .from('users')
-    .update({
-      billing_address: { ...address },
-      payment_method: { ...payment_method[payment_method.type] }
-    })
-    .eq('id', uuid);
-  if (error) throw error;
-};
+// const upsertPriceRecord = async (price: Stripe.Price) => {
+//   const priceData: Price = {
+//     id: price.id,
+//     product_id: typeof price.product === 'string' ? price.product : '',
+//     active: price.active,
+//     currency: price.currency,
+//     description: price.nickname ?? null,
+//     type: price.type,
+//     unit_amount: price.unit_amount ?? null,
+//     interval: price.recurring?.interval ?? null,
+//     interval_count: price.recurring?.interval_count ?? null,
+//     trial_period_days: price.recurring?.trial_period_days ?? null,
+//     metadata: price.metadata
+//   };
+
+//   const { error } = await supabaseAdmin.from('prices').upsert([priceData]);
+//   if (error) throw error;
+//   console.log(`Price inserted/updated: ${price.id}`);
+// };
+
+// // const createOrRetrieveCustomer = async ({
+// //   email,
+// //   uuid
+// // }: {
+// //   email: string;
+// //   uuid: string;
+// // }) => {
+// //   const { data, error } = await supabaseAdmin
+// //     .from('customers')
+// //     .select('stripe_customer_id')
+// //     .eq('id', uuid)
+// //     .single();
+// //   if (error || !data?.stripe_customer_id) {
+// //     // No customer record found, let's create one.
+// //     const customerData: { metadata: { supabaseUUID: string }; email?: string } =
+// //       {
+// //         metadata: {
+// //           supabaseUUID: uuid
+// //         }
+// //       };
+// //     if (email) customerData.email = email;
+// //     const customer = await stripe.customers.create(customerData);
+// //     // Now insert the customer ID into our Supabase mapping table.
+// //     const { error: supabaseError } = await supabaseAdmin
+// //       .from('customers')
+// //       .insert([{ id: uuid, stripe_customer_id: customer.id }]);
+// //     if (supabaseError) throw supabaseError;
+// //     console.log(`New customer created and inserted for ${uuid}.`);
+// //     return customer.id;
+// //   }
+// //   return data.stripe_customer_id;
+// // };
+
+// /**
+//  * Copies the billing details from the payment method to the customer object.
+//  */
+// const copyBillingDetailsToCustomer = async (
+//   uuid: string,
+//   payment_method: Stripe.PaymentMethod
+// ) => {
+//   //Todo: check this assertion
+//   const customer = payment_method.customer as string;
+//   const { name, phone, address } = payment_method.billing_details;
+//   if (!name || !phone || !address) return;
+//   //@ts-ignore
+//   await stripe.customers.update(customer, { name, phone, address });
+//   const { error } = await supabaseAdmin
+//     .from('users')
+//     .update({
+//       billing_address: { ...address },
+//       payment_method: { ...payment_method[payment_method.type] }
+//     })
+//     .eq('id', uuid);
+//   if (error) throw error;
+// };
 
 // const manageSubscriptionStatusChange = async (
 //   subscriptionId: string,
@@ -178,9 +178,8 @@ const copyBillingDetailsToCustomer = async (
 //     );
 // };
 
-export {
-  upsertProductRecord,
-  upsertPriceRecord
-  // createOrRetrieveCustomer,
-  // manageSubscriptionStatusChange
-};
+export // upsertProductRecord,
+// upsertPriceRecord
+// createOrRetrieveCustomer,
+// manageSubscriptionStatusChange
+ {};
